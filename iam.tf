fdbda21c77b32dfd5cab4773bdec33c21a6cb69b
thevanguardian <76273896+thevanguardian@users.noreply.github.com>
2022-04-29 21:44:22 -0400

    Managed policies attribute and associated variable (#2)

--- iam.tf
+++ iam.tf
@@ -3,6 +3,7 @@ resource "aws_iam_role" "generatedRole" {
   path                 = var.rolePath != "" ? var.rolePath : null
   max_session_duration = var.maxSessionDuration
   assume_role_policy   = data.aws_iam_policy_document.generatedAssumePolicy.json
+  managed_policy_arns  = length(var.managedPolicies) > 0 ? var.managedPolicies : null
   inline_policy {
     name   = "GeneratedPolicy"
     policy = data.aws_iam_policy_document.generatedPolicy.json
